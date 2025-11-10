d6f9d263739641dfa6c443c84c58f8e290ed22b1
KhoiiDev <leminhkhoitdtu@gmail.com>
2025-11-10 16:40:23 +0700

    add guards

---
+++ common/guards/jwt-auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from 'src/shared/common/constants/meta-data';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic && !token) {
+      return true;
+    }
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    return super.canActivate(context);
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
---
+++ common/guards/roles-auth.guard.ts
@@ -0,0 +1,60 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import {
+  PERMISSIONS_KEY,
+  ROLES_KEY,
+} from 'src/shared/common/constants/meta-data';
+import { UserPermission } from 'src/shared/core/enums/user-permission.enum';
+import { UserRole } from 'src/shared/core/enums/user-role.enum';
+
+@Injectable()
+export class RolesPermissionsGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    const requiredPermission = this.reflector.getAllAndOverride<
+      UserPermission[]
+    >(PERMISSIONS_KEY, [context.getHandler(), context.getClass()]);
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !user.role || !user.role.permissions) {
+      throw new ForbiddenException('Guard Forbidden');
+    }
+
+    if (user.role.name === UserRole.SUPER_ADMIN) {
+      return true;
+    }
+
+    // Check roles
+    if (requiredRoles.length && !requiredRoles.includes(user.role.name)) {
+      throw new ForbiddenException('Forbidden: role not allowed');
+    }
+
+    // Check permissions
+    if (requiredPermission.length) {
+      const userPermissions = user.role.permissions;
+      const hasAllPermissions = requiredPermission.every((p) =>
+        userPermissions.includes(p),
+      );
+      if (!hasAllPermissions) {
+        throw new ForbiddenException('Forbidden: missing permissions');
+      }
+    }
+
+    return true;
+  }
+}
